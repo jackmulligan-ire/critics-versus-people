41730506b03bee0954f5b247bf3c2f7c5193eb14
Jack Mulligan <mulligja1@gmail.com>
2022-02-27 08:53:49 +0000

    Built initial version of footer component, footer tests

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Logo from './components/Logo/Logo';
 import Motto from './components/Motto/Motto';
 import SearchBar from './components/SearchBar/SearchBar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <main>
-      <Logo />
-      <SearchBar />
-      <Motto />
-    </main>
+    <div id="content">
+      <main>
+        <Logo />
+        <SearchBar />
+        <Motto />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
