c23c62ddb39e491df9228e26784194c97a2da968
Jack Mulligan <mulligja1@gmail.com>
2022-02-27 10:18:34 +0000

    Created ResultsView component, changed App.js to show either ResultsView or InitialView depending on value of a boolean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import InitialView from './components/InitialView/InitialView';
+import ResultsView from './components/ResultsView/ResultsView';
 import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
   render() {
+    const showResults = false;
     return (
       <div id="content">
-        <InitialView />
+        {showResults ? <ResultsView /> : <InitialView />}
         <Footer />
       </div>
     )
---
+++ src/components/ResultsView/ResultsView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ResultsView extends React.Component {
+    render() {
+        return (
+            <div id="results-view">
+                <p>MOVIES!</p>
+            </div>
+        )
+    }
+}
+
+export default ResultsView;
