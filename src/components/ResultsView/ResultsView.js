c9d44a24e8e4529efc0a72072c51bacd60a17529
Jack Mulligan <mulligja1@gmail.com>
2022-03-01 16:44:52 +0000

    Implemented Bootstrap layout of the results page

--- src/components/ResultsView/ResultsView.js
+++ src/components/ResultsView/ResultsView.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import SiteNavbar from '../Navbar/Navbar';
+import SearchBar from '../SearchBar/SearchBar';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class ResultsView extends React.Component {
     render() {
         return (
             <div id="results-view">
                 <SiteNavbar />
+                <Container>
+                    <Row id="search-row">
+                        <SearchBar />
+                    </Row>
+                    <Row id="title-row">
+                        
+                    </Row>
+                    <Row id="review-row">
+                        
+                    </Row>
+                    <Row id="info-row">
+                        
+                    </Row>
+                </Container>
             </div>
         )
     }
