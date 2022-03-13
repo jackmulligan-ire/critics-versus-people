ed845f817bf30dfd5ef4ead1630752f7c6b21767
Jack Mulligan <mulligja1@gmail.com>
2022-03-13 20:24:47 +0000

    Solved img sizing bug at 800-900px

--- src/components/MovieSummary/MovieSummary.js
+++ src/components/MovieSummary/MovieSummary.js
@@ -6,14 +6,14 @@ class MovieSummary extends React.Component {
         const imdbLink = `https://www.imdb.com/title/${this.props.imdbID}/`
         return (
             <Row id="summary-row" className="mt-3 mt-sm-2">
-                <Col md={6} className="p-0 d-flex flex-column justify-content-end summary-para-col">
+                <Col md={6} className="p-0 d-flex flex-column justify-content-center summary-para-col">
                     <p>{this.props.plot}</p>
                     <p>Director: {this.props.director}<br />
                     Actors: {this.props.actors}</p>
                     <p className="m-0">View on <a href={imdbLink} target="_blank">IMDB</a></p>
                 </Col>
                 <Col md={6} className="p-0 mb-3 mb-sm-0 d-flex justify-content-center justify-content-md-end">
-                    <img src={this.props.poster} height="250" width="200"/>
+                    <img className="mx-xxl-5" src={this.props.poster}/>
                 </Col>
             </Row>
         )
