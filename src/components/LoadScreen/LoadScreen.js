f39e3575a8ae599c5c17c9f44c1fd9943ba635b6
Jack Mulligan <mulligja1@gmail.com>
2022-03-15 17:38:41 +0000

    Added loading spinner to load screen

--- src/components/LoadScreen/LoadScreen.js
+++ src/components/LoadScreen/LoadScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Row, Container } from "react-bootstrap";
+import loader from "../../images/white-spinner.gif";
 
 
 class LoadScreen extends React.Component {
@@ -12,14 +13,19 @@ class LoadScreen extends React.Component {
             return json
         }
         fetchMovieData().then(json => {
-            // Setting delay so loading screen is visible for a little bit
+            // Setting delay so loading screen is visible for a little bit 
             setTimeout(() => this.props.onAPIReturn(json), 1000)
         });
     }
 
     render() {
         return (
-            <h1>Loading</h1>
+            <Container className="d-flex flex-column justify-content-center">
+                <Row className="justify-content-center">
+                    <img id="film-reel" src={loader} />
+                    <h5 className="text-center">Peering into telescope to view stars given...</h5>
+                </Row>
+            </Container>
         )
     }
 }
