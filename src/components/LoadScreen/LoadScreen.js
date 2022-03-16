0ead7853af7bbb15da383b397aea587a06c12917
Jack Mulligan <mulligja1@gmail.com>
2022-03-16 09:49:26 +0000

    Rewrote API call to just use async/await

--- src/components/LoadScreen/LoadScreen.js
+++ src/components/LoadScreen/LoadScreen.js
@@ -4,7 +4,7 @@ import loader from "../../images/white-spinner.gif";
 
 
 class LoadScreen extends React.Component {
-    componentDidMount() {
+    async componentDidMount() {
         const fetchMovieData = async () => {
             const movieString = this.props.query.split(" ").join("+");
             const queryString = `http://www.omdbapi.com/?apikey=d6c3d2ef&t=${movieString}`;
@@ -12,10 +12,8 @@ class LoadScreen extends React.Component {
             const json = await response.json();
             return json
         }
-        fetchMovieData().then(json => {
-            // Setting delay so loading screen is visible for a little bit 
-            setTimeout(() => this.props.onAPIReturn(json), 1000)
-        });
+        const json = await fetchMovieData();
+        setTimeout(() => this.props.onAPIReturn(json), 1000)
     }
 
     render() {
